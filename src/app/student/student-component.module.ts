4fae5213d31d842a59218d53a87fd03472064340
Sundar <sundaranbu007@gmail.com>
2022-09-04 13:39:53 +0530

    assignment

---
+++ src/app/student/student-component.module.ts
@@ -0,0 +1,43 @@
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { StudentAddComponent } from './student-add/student-add.component';
+import { StudentEditComponent } from './student-edit/student-edit.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { StudentDetailComponent } from './student-detail/student-detail.component';
+import { StudentDeleteComponent } from './student-delete/student-delete.component';
+
+const routes: Routes = [
+    {
+      path: '',
+      component: StudentListComponent
+    },
+    {
+      path: 'Student/Add',
+      component: StudentAddComponent
+    },
+    {
+        path: 'Student/Edit/:studentId',
+        component: StudentEditComponent
+    },
+    {
+        path: 'Student/Detail/:studentId',
+        component: StudentDetailComponent
+    },
+    {
+      path: 'Student/Delete/:studentId',
+      component: StudentDeleteComponent
+    },
+];
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+  ]
+})
+export class StudentComponentModule{ }
